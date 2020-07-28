68cf4d1bb7df4300dcff28c9bd42fdf56ae0bf64
Lacuna543 <[kusunya@i.ua]>
2020-07-28 19:59:50 +0300

    Added marathons.html, MarathonController, editMarathon

--- src/main/java/com/softserve/edu/controller/MarathonController.java
+++ src/main/java/com/softserve/edu/controller/MarathonController.java
@@ -18,5 +18,28 @@ public class MarathonController {
         return "marathons";
     }
 
-    //TODO implement other needed methods
+    @GetMapping("/marathons/create")
+    public String createMarathons(Model model) {
+        model.addAttribute("marathon", new Marathon());
+        return "createMarathon";
+    }
+
+    @GetMapping("/marathons/edit/{id}")
+    public String editMarathon(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("marathon", marathonService.getMarathonById(id));
+        return "editMarathon";
+    }
+    @PostMapping("/marathons/edit/{id}")
+    public String saveMarathon(@PathVariable("id") Long id, @ModelAttribute Marathon marathon, Model model) {
+        model.addAttribute("marathon", marathonService.createOrUpdate(marathon));
+        return "editMarathon";
+    }
+
+
+
+    @GetMapping("/marathons/close")
+    public String closeMarathons(Model model) {
+        //model.addAttribute("marathons", marathonService.createOrUpdate(marathonService.getId()));
+        return "editMarathon";
+    }
 }
