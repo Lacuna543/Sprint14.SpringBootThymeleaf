e0a4513654f2c2358f75969c7cfcfaf0cc9af81c
Pr0glamer <nickolayladenko89@gmail.com>
2020-07-29 21:43:32 +0300

    All of students controller together

--- src/main/java/com/softserve/edu/Application.java
+++ src/main/java/com/softserve/edu/Application.java
@@ -5,9 +5,7 @@ import com.softserve.edu.model.*;
 import com.softserve.edu.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
@@ -18,42 +16,41 @@ import java.util.Set;
 @SpringBootApplication
 public class Application {
 
-	UserService userService;
-	MarathonService marathonService;
-	private final SprintService sprintService;
-	private final TaskService taskService;
-	private final ProgressService progressService;
+    UserService userService;
+    MarathonService marathonService;
+    private final SprintService sprintService;
+    private final TaskService taskService;
+    private final ProgressService progressService;
 
-	public Application(UserService userService, MarathonService marathonService, SprintService sprintService, TaskService taskService, ProgressService progressService) {
-		this.userService = userService;
-		this.marathonService = marathonService;
-		this.sprintService = sprintService ;
-		this.taskService = taskService;
-		this.progressService = progressService;
-	}
+    public Application(UserService userService, MarathonService marathonService, SprintService sprintService, TaskService taskService, ProgressService progressService) {
+        this.userService = userService;
+        this.marathonService = marathonService;
+        this.sprintService = sprintService;
+        this.taskService = taskService;
+        this.progressService = progressService;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
-	}
+        SpringApplication.run(Application.class, args);
+    }
 
 
-	public void run(String... args) throws Exception {
-		System.out.println("Running Spring Boot Application");
-		/*try{
-			for (int i=0; i < 5; i++) {
-				User user = new User();
-				user.setPassword("qwertyQwerty6^");
-				user.setRole(User.Role.TRAINEE);
-				user.setFirstName("TraineeName" + i);
-				user.setLastName("TraineeSurname" + i);
-				user.setEmail("trainee" + i + "@user.com");
-				userService.createOrUpdateUser(user);
-			}
-		}
-		catch (ConstraintViolationException e){
-			System.out.println(e.getMessage());
-		}*/
+    public void run(String... args) throws Exception {
+        System.out.println("Running Spring Boot Application");
+        /*try {
+            for (int i = 0; i < 5; i++) {
+                User user = new User();
+                user.setPassword("qwertyQwerty6^");
+                user.setRole(User.Role.TRAINEE);
+                user.setFirstName("TraineeName" + i);
+                user.setLastName("TraineeSurname" + i);
+                user.setEmail("trainee" + i + "@user.com");
+                userService.createOrUpdateUser(user);
+            }
+        } catch (ConstraintViolationException e) {
+            System.out.println(e.getMessage());
+        }*/
 
 //		User user = userService.getUserById(1L);
 //		user.setPassword("newNewPassword");
@@ -67,51 +64,48 @@ public class Application {
 //		userService.createOrUpdateUser(user);
 
 
-
-
-
 //
-  //     List<User> users = userService.getAll();
-    //   System.out.println("------USERS----\n"+users);
+   //     List<User> users = userService.getAll();
+     //   System.out.println("------USERS----\n" + users);
 //        //logger.info("User id 2 -> {}", user.get());
 //
-       /*Marathon marathon = new Marathon();
-       marathon.setTitle("Second marathon");
-       Set<User> userSet = new LinkedHashSet<>();
-       userSet.add(users.get(0));
+       /* Marathon marathon = new Marathon();
+        marathon.setTitle("Second marathon");
+        Set<User> userSet = new LinkedHashSet<>();
+        userSet.add(users.get(0));
         //marathon.setMentors(userSet);
- 		marathonService.createOrUpdate(marathon);*/
+        marathonService.createOrUpdate(marathon);*/
 //        //marathonService.deleteMarathonById(2L);
-		//       List<Marathon> marathons = marathonService.getAll();
-		//       marathon = marathons.get(0);
+        //       List<Marathon> marathons = marathonService.getAll();
+        //       marathon = marathons.get(0);
 //        //System.out.println(marathons);
 //
 //        //System.out.println(userService.getAllByRole("mentor"));
-   /*    User mentor = userService.getAllByRole("trainee").get(2);
-       Marathon m = marathonService.getMarathonById(1L);
-       userService.addUserToMarathon(mentor, m);*/
+        /*User mentor = userService.getAllByRole("trainee").get(2);
+        Marathon m = marathonService.getMarathonById(1L);
+        userService.addUserToMarathon(mentor, m);*/
 //        //Set<User> mentors = marathon.getMentors();
 //        //mentors.add(mentor);
 //        //marathonService.createOrUpdate(m);
 //        //System.out.println(marathonService.getAllMentors(marathon));
 //        //userService.addUserToMarathon(mentor, marathon);
-   /*     Sprint sprint = new Sprint();
-		sprint.setTitle("Second sprint");
-		sprint.setStartDate(LocalDate.of(2020, 07, 23));
-		sprint.setEndDate(LocalDate.of(2020, 07, 25));
-		sprintService.addSprintToMarathon(sprint, m);
-        sprint = sprintService.getSprintById(1L);
-        Task task = new Task();
-		task.setTitle("First task for first sprint");
-		taskService.addTaskToSprint(task, sprint);
-		task = taskService.getTaskById(1L);
-		User user = userService.getUserById(4L);
-        progressService.addTaskForStudent(task, user);
-	    Progress progress = progressService.getProgressById(1L);
-        progress.setStatus(Progress.TaskStatus.PENDING);
-		System.out.println("!!!"+progressService.setStatus(Progress.TaskStatus.PENDING, progress));*/
+//        Sprint sprint = new Sprint();
+//        sprint.setTitle("Second sprint");
+//        sprint.setStartDate(LocalDate.of(2020, 07, 23));
+//      //  sprint.setFinish(LocalDate.of(2020, 07, 25));
+//        sprintService.addSprintToMarathon(sprint, m);
+//        sprint = sprintService.getSprintById(1L);
+//        Task task = new Task();
+//        task.setTitle("First task for first sprint");
+//        taskService.addTaskToSprint(task, sprint);
+//        task = taskService.getTaskById(1L);
+//        User user = userService.getUserById(4L);
+//        progressService.addTaskForStudent(task, user);
+//        Progress progress = progressService.getProgressById(1L);
+//        progress.setStatus(Progress.TaskStatus.PENDING);
+//        System.out.println("!!!" + progressService.setStatus(Progress.TaskStatus.PENDING, progress));
 
-	//System.out.println(progressService.allProgressByUserIdAndSprintId(26L, 1L));
+        //System.out.println(progressService.allProgressByUserIdAndSprintId(26L, 1L));
 
-	}
+    }
 }
--- src/main/java/com/softserve/edu/controller/MarathonController.java
+++ src/main/java/com/softserve/edu/controller/MarathonController.java
@@ -1,20 +1,28 @@
 package com.softserve.edu.controller;
 
 
+import com.softserve.edu.model.Marathon;
+import com.softserve.edu.model.User;
 import com.softserve.edu.service.MarathonService;
+import com.softserve.edu.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Data
+@AllArgsConstructor
 public class MarathonController {
     private MarathonService marathonService;
 
     @GetMapping("/marathons")
     public String getAllMarathons(Model model) {
-        //TODO
+        model.addAttribute("marathons", marathonService.getAll());
         return "marathons";
     }
 
